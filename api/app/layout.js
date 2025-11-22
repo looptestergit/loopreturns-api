c047efaa6a036df3314af11246a6c3440d6c0d23
looptestergit <looptester@redsfitness.com>
2025-11-22 15:51:04 -0500

    Add files via upload

---
+++ api/app/layout.js
@@ -0,0 +1,4 @@
+export const metadata = { title: "LoopReturns Dashboard" };
+export default function RootLayout({ children }) {
+  return <html><body className='p-8 bg-gray-100'>{children}</body></html>;
+}
